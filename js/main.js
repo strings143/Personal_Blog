051ef8ab119efd71df30dc76c1ffd1d783fd57b3
unknown <strings@DESKTOP-EEG2V7L.dyu.edu.tw>
2024-04-03 01:39:08 +0800

    修改Project內容

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Project set button 
+//jQuery
+window.onload = function (){
+    const project_img = document.querySelectorAll('.project_img');
+    const project_btn=document.querySelectorAll(".project_btn");
+    for(let i=0;i<project_img.length;i++){
+        project_img[i].addEventListener("mouseenter", event => {
+            project_btn[i].style.visibility="visible";
+            //圖片需要設定模糊
+        });
+        project_img[i].addEventListener("mouseleave", event => {
+            project_btn[i].style.visibility="hidden";
+        });
+    }
+}
+
+
